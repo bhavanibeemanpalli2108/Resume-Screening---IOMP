dedc2fd11f6e9583600f91e3fc1f96c3fa79525e
bhavanibheemanpalli2108 <itsbhavanibheemanpally08@gmail.com>
2026-06-09 05:30:47 +0530

    RSAIT update

--- core - Copy/embeddings.py
+++
@@ -1,5 +0,0 @@
-from models.sbert_model import model
-
-def generate_embedding(text):
-    embedding = model.encode(text)
-    return embedding.tolist()
--- utils - Copy/constants.py
+++
@@ -1,7 +0,0 @@
-SKILL_LIST = [
-    "python", "java", "c++", "sql", "machine learning",
-    "deep learning", "tensorflow", "pytorch", "nlp",
-    "data analysis", "pandas", "numpy", "scikit-learn",
-    "react", "nodejs", "html", "css", "javascript",
-    "aws", "docker", "kubernetes", "git", "linux"
-]
